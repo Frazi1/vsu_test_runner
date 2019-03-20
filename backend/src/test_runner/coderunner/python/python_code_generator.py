3c4d30b422922bf3ed16181b7abd3643299356d2
Fraz <diman896@gmail.com>
2019-03-20 21:58:31 +0300

    [backend] Refactor python_runner to move code generation code to code generator

--- backend/src/test_runner/coderunner/python/python_code_generator.py
+++ backend/src/test_runner/coderunner/python/python_code_generator.py
@@ -3,13 +3,21 @@ from typing import List
 from coderunner.function_run_argument import FunctionRunArgument
 from coderunner.function_run_plan import FunctionRunPlan
 from models.argument_type import ArgumentType
+from models.code_snippet import CodeSnippet
+from models.function import Function
+from models.function_parameter import FunctionArgument
 from shared.value_converter import ValueConverter
 from utils.business_error import BusinessException
 
 
 class PythonCodeGenerator():
-    def generate_function_call_text(self, function_run_plan):
-        # type: (FunctionRunPlan) -> str
+    _indentation = 4
+    _indentation_symbol = " "
+
+    FUNC_DECLARATION_MARKER = "%FUNC_DECLARATION%"
+    FUNC_CALL_MARKER = "%FUNC_CALL%"
+
+    def generate_function_call_text(self, function_run_plan: FunctionRunPlan) -> str:
         arguments_ = [self.generate_type_value_repr_text(x) for x in function_run_plan.arguments]
         return "res = " + self._generate_function_call_inner(function_run_plan.function.name, arguments_)
 
@@ -39,3 +47,65 @@ class PythonCodeGenerator():
             raise BusinessException("Function declaration should start with 'def'")
         decorator_call = self._generate_function_call_inner("@" + decorator_name, decorator_params)
         return decorator_call + func_declaration_code
+
+    def scaffold_function_declaration_text(self, function_: Function) -> str:
+        args = [self._translate_parameter(x) for x in function_.arguments]
+        res = "def {name}({args}):\n".format(name=function_.name,
+                                             args=", ".join(args))
+        res += self._get_indent() + self._get_commented_text("Write code here")
+
+        return res
+
+    def _translate_parameter(self, argument: FunctionArgument) -> str:
+        return argument.name
+
+    def _get_indent(self) -> str:
+        return self._indentation * self._indentation_symbol
+
+    def _get_commented_text(self, text):
+        return "# {}".format(text)
+
+    def _indent_line(self, line, last):
+        res = " " * self._indentation + line
+        if last is not True:
+            res += "\n"
+        return res
+
+    def _add_fixed_indent(self, code: str, level: int) -> str:
+        lines = code.split("\n")
+        for index in range(0, len(lines)):
+            lines[index] = self._get_indent() * level + lines[index]
+        return "\n".join(lines)
+
+    def _add_indent(self, code: str, spaces: int) -> str:
+        lines = [x for x in code.split("\n") if x != ""]
+        for index in range(0, len(lines)):
+            lines[index] = self._indentation_symbol * spaces + lines[index]
+        return "\n".join(lines)
+
+    def translate_code(self, function_signature: Function, code_snippet: CodeSnippet) -> str:
+        parameters = [self._translate_parameter(x) for x in function_signature.parameters]
+        signature = "def {name}({parameters}):\n".format(name=function_signature.name,
+                                                         parameters=", ".join(parameters))
+
+        code_lines = code_snippet.code.splitlines()
+        formatted_code = "".join(
+            [(self._indent_line(x, index == len(code_lines) - 1)) for index, x in enumerate(code_lines)])
+        return signature + formatted_code
+
+    def prepare_code_from_template(self, template_text, function_run_plans: List[FunctionRunPlan],
+                                   function_declaration_code: str) -> str:
+        indent = next(line for line in template_text.split("\n") if self.FUNC_CALL_MARKER in line).index(
+            self.FUNC_CALL_MARKER[0])
+
+        function_calls_code = [self.generate_function_call_text(plan) for plan in function_run_plans]
+        function_calls_code_indented = [self._add_indent(call_code, indent) for call_code in function_calls_code]
+        ready_template = template_text.replace(indent * self._indentation_symbol + self.FUNC_CALL_MARKER,
+                                               "\n".join(function_calls_code_indented))
+
+        result_function_declaration = self.add_decorator(function_declaration_code, "__notify_iteration", [])
+        result_function_declaration = self._add_indent(result_function_declaration, indent)
+
+        ready_template = ready_template.replace(indent * self._indentation_symbol + self.FUNC_DECLARATION_MARKER,
+                                                result_function_declaration)
+        return ready_template
--- backend/src/test_runner/coderunner/python/python_runner.py
+++ backend/src/test_runner/coderunner/python/python_runner.py
@@ -8,16 +8,12 @@ from coderunner.python.python_code_generator import PythonCodeGenerator
 from coderunner.simple_runner import SimpleRunner
 from dtos.dtos import CodeRunResult
 from models.argument_type import ArgumentType
-from models.function import Function
-from models.function_parameter import FunctionArgument
 from models.language_enum import LanguageEnum
 from shared.value_converter import ValueConverter
 
 
 class PythonRunner(SimpleRunner):
     __file_ext__ = ".py"
-    _indentation = 4
-    _indentation_symbol = " "
 
     @property
     def supported_language(self):
@@ -28,48 +24,6 @@ class PythonRunner(SimpleRunner):
     def __init__(self, config):
         # type: (Config) -> None
         super(PythonRunner, self).__init__(config)
-        self.FUNC_DECLARATION_MARKER = "%FUNC_DECLARATION%"
-        self.FUNC_CALL_MARKER = "%FUNC_CALL%"
-
-    def _translate_parameter(self, argument):
-        # type: (FunctionArgument) -> str
-        return argument.name
-
-    def _get_indent(self):
-        # type: () -> str
-        return self._indentation * self._indentation_symbol
-
-    def _get_commented_text(self, text):
-        return "# {}".format(text)
-
-    def _indent_line(self, line, last):
-        res = " " * self._indentation + line
-        if last is not True:
-            res += "\n"
-        return res
-
-    def _add_fixed_indent(self, code, level):
-        # type:(str, int) -> str
-        lines = code.split("\n")
-        for index in range(0, len(lines)):
-            lines[index] = self._get_indent() * level + lines[index]
-        return "\n".join(lines)
-
-    def _add_indent(self, code: str, spaces: int) -> str:
-        lines = [x for x in code.split("\n") if x != ""]
-        for index in range(0, len(lines)):
-            lines[index] = self._indentation_symbol * spaces + lines[index]
-        return "\n".join(lines)
-
-    def translate_code(self, function_signature, code_snippet):
-        parameters = [self._translate_parameter(x) for x in function_signature.parameters]
-        signature = "def {name}({parameters}):\n".format(name=function_signature.name,
-                                                         parameters=", ".join(parameters))
-
-        code_lines = code_snippet.code.splitlines()
-        formatted_code = "".join(
-            [(self._indent_line(x, index == len(code_lines) - 1)) for index, x in enumerate(code_lines)])
-        return signature + formatted_code
 
     def _parse_out_file(self, out: str, language: LanguageEnum, return_type: ArgumentType) -> List[CodeRunResult]:
         regex = re.compile(
@@ -99,27 +53,7 @@ class PythonRunner(SimpleRunner):
         with open(self._config.python_default_template_path, "r") as file_:
             template_text = file_.read()
 
-        indent = next(line for line in template_text.split("\n") if self.FUNC_CALL_MARKER in line).index(
-            self.FUNC_CALL_MARKER[0])
-
-        function_calls_code = [self.code_generator.generate_function_call_text(plan) for plan in function_run_plans]
-        function_calls_code_indented = [self._add_indent(call_code, indent) for call_code in function_calls_code]
-        ready_template = template_text.replace(indent * self._indentation_symbol + self.FUNC_CALL_MARKER,
-                                               "\n".join(function_calls_code_indented))
-
-        result_function_declaration = self.code_generator.add_decorator(function_declaration_code,
-                                                                        "__notify_iteration", [])
-        result_function_declaration = self._add_indent(result_function_declaration, indent)
-
-        ready_template = ready_template.replace(indent * self._indentation_symbol + self.FUNC_DECLARATION_MARKER,
-                                                result_function_declaration)
-
+        ready_template = self.code_generator.prepare_code_from_template(template_text,
+                                                                        function_run_plans,
+                                                                        function_declaration_code)
         return self.execute_plain_code(function_run_plans[0].function.return_type, ready_template)
-
-    def scaffold_function_declaration_text(self, function_: Function) -> str:
-        args = [self._translate_parameter(x) for x in function_.arguments]
-        res = "def {name}({args}):\n".format(name=function_.name,
-                                             args=", ".join(args))
-        res += self._get_indent() + self._get_commented_text("Write code here")
-
-        return res

297884f20b3338fd0ebd4bfad2bfdf12e7c5bf13
Fraz <diman896@gmail.com>
2019-03-31 14:30:04 +0300

    [frontend] CSharp code execution performance test

--- backend/src/tests/performance/Simple.py
+++ backend/src/tests/performance/Simple.py
@@ -7,6 +7,35 @@ class RestTasks(TaskSet):
         self.client.get("/template")
 
     @task
+    def code_execution_csharp_print(self):
+        self.client.post("/code/run",
+                         json={"clientId": None,
+                               "returnType": {"name": "STRING"},
+                               "functionId": None,
+                               "language": {"name": "CSHARP"},
+                               "code": """using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Feistel
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var rnd = new Random();
+            string message = "Test 1 Test 2 Test 3 Test 4 Test 5 Test 6 Test 7";
+//Console.WriteLine(message);
+            Console.WriteLine($"Decrypted text: {message}");
+
+        }
+    }
+}""",
+                               "executionType": "ONE_BY_ONE"}
+                         )
+
+    @task
     def code_execution_python_print(self):
         self.client.post("/code/run",
                          json={"clientId": None,
@@ -16,6 +45,7 @@ class RestTasks(TaskSet):
                                "code": "def testF():\n    print '100'\ntestF()",
                                "isPlainCode": True}
                          )
+
 
 class User(HttpLocust):
     task_set = RestTasks
